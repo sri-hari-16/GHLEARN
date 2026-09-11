9889d50107e6aa7a3b0230569426055aefc13127
SRI HARI M <sriharimaghendran@gmail.com>
2026-09-11 22:11:53 +0530

    Third commit

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //creating the scanner object
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the Number you want square root of :");
         double number = sc.nextDouble();
