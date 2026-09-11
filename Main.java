6d09b00e986606a76a4709dd58b51acbe3289af1
SRI HARI M <sriharimaghendran@gmail.com>
2026-09-11 22:23:31 +0530

    refactored the codebase

--- Main.java
+++ Main.java
@@ -1,13 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
+    private static void squareRootGenerator(double number)
+    {
+        double result = Math.sqrt(number);
+        System.out.println("The Square Root of the Number is:"+result);
+    }
     public static void main(String[] args) {
         //creating the scanner object
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the Number you want square root of :");
         double number = sc.nextDouble();
-        double result = Math.sqrt(number);
-        System.out.println("The Square Root of the Number is:"+result);
+        squareRootGenerator(number);
         sc.close();//closing the scanner object
     }
 }
