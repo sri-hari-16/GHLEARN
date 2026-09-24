cab91040bf33e9fbccf5f92acb18ef8b22b6c40c
SRI HARI M <sriharimaghendran@gmail.com>
2026-09-25 00:09:44 +0530

    added multiply functionality

--- Main.java
+++ Main.java
@@ -6,12 +6,17 @@ public class Main {
         double result = Math.sqrt(number);
         System.out.println("The Square Root of the Number is:"+result);
     }
+    private static void multiplyByItself(double number)
+    {
+        System.out.println("The Multiple is : "+(number * number));
+    }
     public static void main(String[] args) {
         //creating the scanner object
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the Number you want square root of :");
         double number = sc.nextDouble();
         squareRootGenerator(number);
+        multiplyByItself(number);
         sc.close();//closing the scanner object
     }
 }
