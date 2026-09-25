bc5da0d787f32bc1de2b2128f861498ab7f940d0
SRI HARI M <sriharimaghendran@gmail.com>
2026-09-26 00:17:32 +0530

    improvised the sub feature

--- SubFeature.java
+++ SubFeature.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class SubFeature {
+    private static void moduloValue(int number1 ,int number2)
+    {
+        System.out.println("The value is : " + (number1 % number2));
+    }
     public static void main(String[] args) {
-        System.out.println("I'm Loving to code in JAVA...");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number one:");
+        int number1 = sc.nextInt();
+        System.out.print("Enter the number two:");
+        int number2 = sc.nextInt();
+        moduloValue(number1, number2);
+        sc.close();
     }
 }
